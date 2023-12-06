d8c4d4b50d50b92926898699d3aa5e690ed303d1
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-12-06 16:39:08 -0500

    background added...imgs are broken

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+
+
 
 function App() {
   return (
     <div className="App">
     <>
+    {/* beginning of Navbar */}
       <Navbar className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">
@@ -18,6 +23,17 @@ function App() {
               className="d-inline-block align-top"
             />{' '}
           </Navbar.Brand>
+          {/* location search form */}
+          <Form inline style={{"width":"60vw"}}>
+            <InputGroup>
+                <Form.Control
+                  placeholder="Search products and stores"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
+            </InputGroup>
+          </Form>
+          {/* login/setup links */}
           <Nav className="justify-content-end" activeKey="/home">
             <Nav.Item>
               <Nav.Link href="/home">Login</Nav.Link>
@@ -29,10 +45,21 @@ function App() {
         </Container>
       </Navbar>
     </> 
-  
+    {/* Background banner */}
+      <div className="Card">
+      <img 
+          className='Img'
+          src='/Produce.avif'
+          alt='Produce wall'
+          />
+        <h1>Order groceries for</h1>
+        <h1>delivery or pickup today</h1>
+        <h2>Whatever you want from local stores, brought right to your door.</h2>
+      </div>
+      {/* Store Locations */}
 
-      <h1>Hello World</h1>
     </div>
+    
   );
 }
 
